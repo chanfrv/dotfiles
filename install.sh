c6f7b068a55460496488627af411d4b6e08e182d
Victor Chanfrault <victor.chanfrault@epita.fr>
2018-02-11 15:41:43 +0100

    install: this is getting out of hand

--- install.sh
+++ install.sh
@@ -1,53 +1,98 @@
 #!/bin/sh
 
+#
+# Usage
+#
 if [ $# -ne 0 ]; then
   echo "Usage: ./install.sh"
   exit 1
 fi
 
+#
+# Tracked dotfiles
+#
 DOTS="config/i3 config/polybar bashrc vimrc Xresources"
-
-#
-# Money
-#
-echo -e "You are on install.sh script.\n"
-echo "Here are the tracked dotfiles:"
-for f in $DOTS; do
-  echo $f
-done
-echo -e "\nTo copy your tracked file, press 1."
-echo -e "To abort the copy, press 2."
-echo -e "\nCongratulations, your are the 1000th to open this script!"
-echo -e "Enter # followed by your birth date to win a brand new iPhone.\n"
-
-echo -n "> "
-while read -r line; do
-  if [ "$line" == "1" ]; then
-    break
-  elif [ "$line" == "2" ]; then
-    echo "Coward."
-    exit 2
-  elif [ "${line:0:1}" == "#" ]; then
-    echo "Congratulations, you have been selected!"
-    echo "Send a mail containing your credit card codes to no-bamboozle@install.sh."
-    echo "Thank you for your contribution!"
-    exit 0
-  else
-    exit 1
-  fi
-done<&0
 
 #
 # Actual script
 #
-for file in $DOTS; do
-  opath="$HOME/.$file"
-  dpath="$file"
+copy()
+{
+  for file in $DOTS; do
+    opath="$HOME/.$file"
+    dpath="$file"
 
-  if [ -d "$opath" ]; then
-    dpath="${dpath%%/*}/"
+    if [ -d "$opath" ]; then
+      dpath="${dpath%%/*}/"
+    fi
+
+    echo "Copy of $opath to $dpath..."
+    cp -r "$opath" "$dpath"
+  done
+  exit 0
+}
+
+
+add_files()
+{
+  echo "Enter the files separated with a space."
+
+  echo -n "> "
+  read line
+  echo
+
+  DOTS="$DOTS $line"
+
+  main
+}
+
+coward()
+{
+  echo "Coward."
+  exit 2
+}
+
+#
+# Money
+#
+you_have_been_bamboozled()
+{
+  echo "Congratulations, you have been selected!"
+  echo "Send a mail containing your credit card codes to no-bamboozle@install.sh."
+  echo "Thank you for your contribution!"
+  exit 0
+}
+
+main()
+{
+  echo -e "=== You are on install.sh script. ===\n"
+  echo "Here are the tracked dotfiles:"
+  for f in $DOTS; do
+    echo $f
+  done
+
+  echo    "To definitively add tracked file, please add then in \`DOTS\'"
+  echo -e "directly by editing this script.\n"
+  echo -e "\nTo copy your tracked file, press 1."
+  echo    "To temporary add tracked files, press 2."
+  echo -e "To abort the copy, press 3.\n"
+  echo    "Congratulations, your are the 1000th to open this script!"
+  echo -e "Enter # followed by your birth date to win a brand new iPhone.\n"
+
+  echo -n "> "
+  read line
+  echo
+  if [ "$line" == "1" ]; then
+    copy
+  elif [ "$line" == "2" ]; then
+    add_files
+  elif [ "$line" == "3" ]; then
+    coward
+  elif [ "${line:0:1}" == "#" ]; then
+    you_have_been_bamboozled
+  else
+    exit 1
   fi
+}
 
-  echo "Copy of $opath to $dpath..."
-  cp -r "$opath" "$dpath"
-done
+main
